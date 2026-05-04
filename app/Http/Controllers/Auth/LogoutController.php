ad5e5efe7ba0074e9f168153818cc94a4decc451
Giorgian Robescu <robescu.george96@gmail.com>
2026-05-04 10:21:19 +0300

    Added login

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -13,8 +13,9 @@ class LogoutController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $user = $request->user();
-        Auth::logout($user);
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
         return redirect()->route('home')->with('success', "You've logged out");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\BlipController;
@@ -17,4 +18,6 @@ Route::middleware('auth')->group(function() {
 
 Route::view('/register', 'auth.register')->middleware('guest')->name('register');
 Route::post('/register', RegisterController::class)->middleware('guest');
-Route::post('/logout', LogoutController::class)->middleware('auth');
+Route::post('/logout', LogoutController::class)->middleware('auth')->name('logout');
+Route::view('/login', 'auth.login')->middleware('guest')->name('login');
+Route::post('/login', LoginController::class)->middleware('guest');
